f26ed8de97b16b8253c22f48dbaedc4b0c9aa49a
scottenvoy <scott@weareenvoy.com>
2018-01-26 00:13:16 -0800

    setting up form inputs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="container">
+        <h3>Enter your email and let's get started with your profile!</h3>
+        <div className="app__inner">
           <Form/>
         </div>
       </div>
@@ -16,9 +17,22 @@ class App extends Component {
 class Form extends Component {
   render() {
     return (
-      <form className="form-group">
-        <input className="form-control" type="email" placeholder="jane@mac.com"></input>
+      <div className="form-inner">
+      <form className="app__form-group form-group">
+        <div className="app__form-step app__form-step--1">
+          <input className="form-control" type="email" placeholder="jane@gmail.com"></input>
+        </div>
+        <div className="app__form-step app__form-step--2">
+          <label><input type="checkbox"/>Rock</label>
+          <label><input type="checkbox"/>Hip-Hop</label>
+          <label><input type="checkbox"/>Electronic</label>
+          <label><input type="checkbox"/>Country</label>
+          <label><input type="checkbox"/>Funk</label>
+          <label><input type="checkbox"/>Classical</label>
+        </div>
       </form>
+      <button type="button" class="btn btn-light btn-lg">Lets go!</button>
+      </div>
     )
   }
 }
